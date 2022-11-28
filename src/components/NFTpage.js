7ca6db15a28d2b1234f377c6e6cd2a143bcae8c6
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-11-28 14:52:26 +0100

    fixed buy nft issues.

--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -94,8 +94,8 @@ async function buyNFT(tokenId) {
                     </div>
                     <div>
                     { currAddress == data.owner || currAddress == data.seller ?
+                        <div className="text-emerald-700">You are the owner of this NFT</div>:
                         <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" onClick={() => buyNFT(tokenId)}>Buy this NFT</button>
-                        : <div className="text-emerald-700">You are the owner of this NFT</div>
                     }
                     
                     <div className="text-green text-center mt-3">{message}</div>
